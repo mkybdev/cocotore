990fa89638721307aaaebdfefccb7e039c4f9455
mkybdev <mitukouyb@gmail.com>
2023-10-21 00:07:31 +0900

    game: pre-release

--- public/game.js
+++ public/game.js
@@ -12,28 +12,34 @@ function makeCard() {
         ["ウサギ", "素早さが次のターンに2上昇する", "rabbit"],
         ["ハリネズミ", "このスキル発動時にHPが半分以下になっていた場合、次のターンから防御力が10上がる", "hedgehog"]
     ];
+    let n = 1;
     const chooseAnimal = () => {
-        document.querySelector('.message').innerHTML = '動物を選んでください';
-        document.querySelector('.content').innerHTML = '<table class="animals"></table>';
-        const table = document.querySelector('.animals');
-        const tbody = document.createElement('tbody');
-        for (let i = 0; i < 2; i++) {
-            const row = document.createElement("tr");
-            for (let j = 0; j < 5; j++) {
-                const idx = 5*i + j;
-                const cell = document.createElement("td");
-                cell.addEventListener('click', () => {
-                    showImage(idx);
-                });
-                cell.innerHTML = `<img src="./images/animals/${animalList[idx][2]}.png" style="width: 100%;"><p style="font-size: 1.2rem; margin: 0.5rem 0;">${animalList[idx][0]}</p>`;
-                row.appendChild(cell);
-            }
-            tbody.appendChild(row); 
-        }  
-        table.appendChild(tbody);
+        if (n == 3)
+            prepare(0);
+        else {
+            document.querySelector('.message').innerHTML = `${n}人目のカードを作ります。<br>まず、あなたがよく感じるネガティブな気持ちを思いうかべてください。<br>次に、そのイメージにいちばん合う動物を選んでください。`;
+            document.querySelector('.content').innerHTML = '<table class="animals"></table>';
+            const table = document.querySelector('.animals');
+            const tbody = document.createElement('tbody');
+            for (let i = 0; i < 2; i++) {
+                const row = document.createElement("tr");
+                for (let j = 0; j < 5; j++) {
+                    const idx = 5*i + j;
+                    const cell = document.createElement("td");
+                    cell.addEventListener('click', () => {
+                        showImage(idx);
+                    });
+                    cell.innerHTML = `<img src="./images/animals/${animalList[idx][2]}.png" style="width: 80%;"><p style="font-size: 1.2rem; margin: 0.5rem 0;">${animalList[idx][0]}</p>`;
+                    row.appendChild(cell);
+                }
+                tbody.appendChild(row); 
+            }  
+            table.appendChild(tbody);
+        }
     }
+    window.chooseAnimal = chooseAnimal;
     const showImage = (idx) => {
-        document.querySelector('.message').innerHTML = 'なまえとせいかくを決めて、下のようにカードを作ってください<br>※HP・こうげき・すばやさはまだ書かないでね!';
+        document.querySelector('.message').innerHTML = 'えらんだ動物のなまえとせいかくを決めて<br>下のようにカードを作ってください（例: イライラトラ）<br>※HP・こうげき・すばやさはまだ書かないでね!スキルは下にあるものを写してね!';
         document.querySelector('.content').innerHTML = '<div class="card"></div>';
         const card = document.querySelector('.card');
         card.innerHTML += '<div style="display: flex; width: 80%; justify-content: space-between; margin: 2rem 0 0.5rem 0;"><p style="margin: 0;">HP</p><p style="margin: 0;">こうげき</p><p style="margin: 0;">すばやさ</p></div>';
@@ -42,24 +48,33 @@ function makeCard() {
         card.innerHTML += `<div style="width: 80%;"><p style="margin: 0;">せいかく</p><div style="height: 10dvh;"></div>`;
         document.querySelector('.content').innerHTML += '<input type="button" class="btn" onclick="setParams();" value="次へ">';
     }
+    const attrList = ["火", "水", "木", "光", "闇"];
     const setParams = () => {
-        document.querySelector('.message').innerHTML = 'パラメータと属性は次のように決まりました！';
-        document.querySelector('.content').innerHTML = '<div style="font-size: 1.5rem; text-align: center;"><p>HP : 25</p><p>こうげき : 10</p><p>すばやさ : 15</p><p>属性 : 闇</p></div>';
-        document.querySelector('.content').innerHTML += '<input type="button" class="btn" onclick="prepare(0);" value="次へ">';
+        let hp = -1, atk = -1, spd = -1;
+        while (hp*atk*spd <= 0) {
+            hp = Math.floor(Math.random() * (50 - 20) + 20);
+            atk = Math.floor(Math.random() * ((50-hp) - 0) + 0);
+            spd = 50 - hp - atk;
+        }
+        const attr = Math.floor(Math.random() * 5);
+        document.querySelector('.message').innerHTML = 'HP・こうげき・すばやさと属性は次のように決まりました!';
+        document.querySelector('.content').innerHTML = `<div style="font-size: 1.5rem; text-align: center;"><p>HP : ${hp}</p><p>こうげき : ${atk}</p><p>すばやさ : ${spd}</p><p>属性 : ${attrList[attr]}</p></div>`;
+        n++;
+        document.querySelector('.content').innerHTML += '<input type="button" class="btn" onclick="chooseAnimal();" value="次へ">';
     };
     window.setParams = setParams;
-    const prepare = (n) => {
+    const prepare = () => {
         if (n == 0) {
-            document.querySelector('.message').innerHTML = 'お互いにカードを作りましたか？';
+            document.querySelector('.message').innerHTML = 'プレイヤー全員がカードを作りましたか？';
             document.querySelector('.content').innerHTML = '<input type="button" class="btn" onclick="prepare(1);" value="はい">';
         } else if (n == 1) {
-            document.querySelector('.message').innerHTML = '作成したカードを交換してください';
+            document.querySelector('.message').innerHTML = '作成したカードをいっしょに遊ぶプレイヤーと交換してください';
             document.querySelector('.content').innerHTML = '<input type="button" class="btn" onclick="prepare(2);" value="次へ">';
         } else if (n == 2) {
             document.querySelector('.message').innerHTML = 'カードを3枚山札から引いてください';
             document.querySelector('.content').innerHTML = '<input type="button" class="btn" onclick="prepare(3);" value="次へ">';
         } else {
-            document.querySelector('.message').innerHTML = '手札から戦闘に参加させるカードを選んでください<br>「ゲームスタート」をクリックするとゲームをスタートします!';
+            document.querySelector('.message').innerHTML = '手札から戦闘に参加させるカードを選んで、ボードに置いてください<br>「ゲームスタート」をクリックするとゲームをスタートします!';
             document.querySelector('.content').innerHTML = '<input type="button" class="btn" onclick="game(true);" value="ゲームスタート">';
         }
     };
@@ -72,14 +87,13 @@ function game(flag) {
     if (flag)
         turn(0);
     else {
-        document.querySelector('.message').innerHTML = 'ゲーム終了！';
-        document.querySelector('.content').innerHTML = '<div style="font-size: 1.5rem; text-align: center;"><p>結果 : 勝利</p><p>スコア : 120</p></div>';
-        document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="location.href='./index.html'" value="トップに戻る">`;
+        document.querySelector('.message').innerHTML = 'ゲーム終了!';
+        document.querySelector('.content').innerHTML = `<input type="button" class="btn" onclick="location.href='./index.html'" value="トップに戻る">`;
     }
 }
 window.game = game;
 
-// 上位ターン
+// ラウンド
 function turn(t) {
     if (t == 3)
         game(false);
@@ -89,18 +103,18 @@ function turn(t) {
         document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="phase(${t}, 0);" value="いいえ">`;
         document.getElementById('dice').addEventListener('click', () => {
             document.querySelector('.message').innerHTML = 'サイコロを振ってください';
-            document.querySelector('.content').innerHTML = '1〜3の場合は、サイコロを振った人のターン、4〜6の場合は振っていない人あるいは敵のターンから始める。<br>3体とも同じ素早さだった場合、1〜2でサイコロを振った人、3〜4で振っていない人、5〜6で敵のターンから始める。<br>そのターンを終えたらもう一回サイコロを振って残りの2人の順番を決める。<br>終わったら「次へ」を押してください<br>';
+            document.querySelector('.content').innerHTML = '素早さが同じカードが2体のとき: 出た目が1〜3の場合は、サイコロを振った人のカードのターンから始める。<br>4〜6の場合はもう一方のカードのターンから始める。<br>素早さが同じカードが3体のとき: 1〜2でサイコロを振った人、3〜4で振っていない人、<br>5〜6で敵のラウンドから始める。<br>次のターンでもう一回サイコロを振って残りの2人の順番を決める。<br>終わったら「次へ」を押してください<br>';
             document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="phase(${t}, 0);" value="次へ">`;
         });
     }
 }
 
-// 下位ターン
+// ターン
 function phase(t, p) {
     if (p == 3)
         turn(t+1);
     else {
-        document.querySelector('.message').innerHTML = '下位ターン<br>敵のターンかプレイヤーのターンかを選択してください';
+        document.querySelector('.message').innerHTML = '次はどちらのターン？';
         document.querySelector('.content').innerHTML = `<input type="button" class="btn" onclick="enemy(${t}, ${p})" value="敵のターン">`;
         document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="player(${t}, ${p})" value="プレイヤーのターン">`;
     }
@@ -110,7 +124,7 @@ window.phase = phase;
 // 敵のターン
 function enemy(t, p) {
     document.querySelector('.message').innerHTML = '敵のターン';
-    document.querySelector('.content').innerHTML = '敵行動カードをプレイヤーのどちらかが引いてください<br>カードの内容に基づいて敵を行動させてください<br>';
+    document.querySelector('.content').innerHTML = '敵行動カードをプレイヤーのどちらかが引いてください<br>カードにある行動を敵がやってきます!<br>';
     document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="phase(${t}, ${p+1})" value="敵のターン終了">`;
 }
 window.enemy = enemy;
@@ -123,22 +137,27 @@ function player(t, p) {
         document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="charachange()" value="次へ">`;
     }
     const charachange = () => {
-        document.querySelector('.content').innerHTML = 'キャラクターを交代する場合は交代するキャラクターをフィールドに召喚してください<br>';
+        document.querySelector('.content').innerHTML = '新しいカードを戦闘に参加させたい場合は今のカードを手札に戻し、新しいカードをフィールドに召喚してください<br>';
         document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="equip()" value="次へ">`;
     }
     window.charachange = charachange;
     const equip = () => {
-        document.querySelector('.content').innerHTML = '武器をキャラクターに付与して質問をしてください<br>終わったら武器の効果を発動してください<br>※使った武器は山札に戻してください<br>';
-        document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="skill()" value="次へ">`;
+        document.querySelector('.content').innerHTML = '武器を自分のキャラクターに付与してください<br>';
+        document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="equipq()" value="次へ">`;
     }
     window.equip = equip;
+    const equipq = () => {
+        document.querySelector('.content').innerHTML = '味方プレイヤーに武器について質問をしてください<br>相手のプレイヤーは答えても答えなくてもよいですが、もし答えてもらえたら<br>感謝を伝えたり相手の話を引き出す質問をしてみましょう!<br>※例: それを共有してくれて、ありがとう。 / あなたの気持ちや考えを理解しようと思います。 / そうなんですね。<br>どうしてそう感じましたか？ / その後、どうなりましたか？<br>';
+        document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="skill()" value="次へ">`;
+    }
+    window.equipq = equipq;
     const skill = () => {
-        document.querySelector('.content').innerHTML = 'キャラクターのスキルを発動してください<br>';
+        document.querySelector('.content').innerHTML = '武器の効果とキャラクターのスキルを発動してください<br>※使った武器は山札に戻してください<br>';
         document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="attack()" value="次へ">`;
     }
     window.skill = skill;
     const attack = () => {
-        document.querySelector('.content').innerHTML = '敵に攻撃してください<br>※素早さが敵の2倍以上の場合、2回攻撃できます<br>';
+        document.querySelector('.content').innerHTML = '敵に攻撃してください<br>※素早さが敵の2倍以上の場合、2回攻撃できます<br><img src="./images/attr.png" style="width: 40dvw;"><br>ダメージ計算方法:<br>キャラクターの攻撃力×スキルの効果(前ターンの効果が継続している場合はそれも含む)<br>×武器の効果×属性の相性(相性が良いと2倍、悪いと0.5倍)<br>';
         document.querySelector('.content').innerHTML += `<input type="button" class="btn" onclick="phase(${t}, ${p+1})" value="プレイヤーのターン終了">`;
     }
     window.attack = attack;
